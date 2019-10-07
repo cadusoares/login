b962eeceb6e8b5f1bb1ebb6841fe07e46c8061b6
cadusoares <carlos_soares@estudante.sc.senai.br>
2019-10-07 18:53:45 -0300

    inicio aula05
    0710/2019

---
+++ receber_dados.php
@@ -0,0 +1,22 @@
+//Teste se existe a ação
+
+if(isset($_POST['action']))
+
+    //Teste se ação é igual a cadastro
+
+    if($_POST['action']= 'cadastro'){
+
+        //Teste se ação é igual a cadastro
+
+    }else if($_POST['action']='login'){
+
+        //Senão, teste se ação é login
+
+    }else if($_POST['action']='senha'){
+
+        //Senão,teste se ação é recuperar senha
+
+    }else{
+        echo "<h1>Alo ha!</h1> <h2>Acesso negado</h2>";
+    }
+
