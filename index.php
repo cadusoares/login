1de4aaac476925602d5c2c48029f1508232e2d1d
cadusoares <carlos_soares@estudante.sc.senai.br>
2019-10-07 21:39:16 -0300

    Envio da dados via AJax e BD
    - Envio de dados dos formulários de login e senha através de Ajax
    -Criação do banco de dados em arquivo SQl

--- index.php
+++ index.php
@@ -1,22 +1,18 @@
 <!doctype html>
 <html lang="pt-br">
-<!-- 
-    Para Acessar o projeto, habilite o Apache no XAMPP 
-    abra a URL  http://localhost/login/  no navegador FireFox (AZUL)
+<!--    
+    Para acessar o projeto, habilite o Apache no XAMPP
+    Abra a URL http://localhost/login/ no navegador
 -->
 
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
     <title>Sistema de Login</title>
-
     <link rel="stylesheet" href="https://jqueryvalidation.org/files/demo/site-demos.css">
-
     <style>
         #alerta,
         #caixaSenha,
@@ -24,275 +20,234 @@
             display: none;
         }
     </style>
-
 </head>
 
 <body class="bg-dark">
     <!-- Fundo Escuro -->
-
     <main class="container mt-4">
-
         <section class="row">
             <div class="col-lg-4 offset-lg-4" id="alerta">
                 <div class="alert alert-success text-center">
                     <strong class="resultado">
-                        EAIIIIIIII
+                        Alo Ha Tchurmaáaaaaaaaaaaaaaaa!
                     </strong>
                 </div>
             </div>
         </section>
-
-        <!-- Inicio Formulário de Login -->
-
+        <!-- Formulário de Login -->
         <section class="row">
-
             <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
-
                 <h2 class="text-center mt-2">
-                    Entrar no Sistema.
+                    Entrar no Sistema
                 </h2>
-
                 <form action="#" method="post" class="p-2" id="formLogin">
-
                     <div class="form-group">
-
                         <input type="text" name="nomeUsuario" id="nomeUsuario" placeholder="Nome de Usuário" class="form-control" required minlength="5">
-
                     </div>
-
                     <div class="form-group">
-
                         <input type="password" name="senhaUsuario" id="senhaUsuario" placeholder="Senha" class="form-control" required minlength="6">
-
                     </div>
-
                     <div class="form-group mt-5">
-
                         <div class="custom-control custom-checkbox">
-
                             <input type="checkbox" name="lembrar" id="lembrar" class="custom-control-input">
-
                             <label for="lembrar" class="custom-control-label">
-                                Lembrar de min.
+                                Lembrar de mim.
                             </label>
-
                             <a href="#" class="float-right" id="btnEsqueci">
                                 Esqueci a senha!
                             </a>
-
                         </div>
                     </div>
-
                     <div class="form-group">
-
-                        <input type="submit" value="Entrar" name="btnEntrar" id="btnEntrar" class="btn btn-primary btn-block">
-
+                        <input type="submit" value="::Entrar::" name="btnEntrar" id="btnEntrar" class="btn btn-primary btn-block">
                     </div>
-
                     <div class="form-group">
-
                         <p class="text-center">
-                            Novo Usuário? <a href="#" id="btnRegistrarNovo">Registre-se aqui!</a>
+                            Novo usuário? <a href="#" id="btnRegistrarNovo">
+                                Registre-se aqui!
+                            </a>
                         </p>
-
                     </div>
-
                 </form>
-
             </div>
-
         </section>
-
-        <!-- Final da Seção de Login -->
-
-
-        <!-- Inicio Formulario de Recuperação de Senha -->
-
+        <!-- Final da Seção de Login-->
+        <!-- Formulário de Recuperação de Senha -->
         <section class="row mt-5">
-
             <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaSenha">
-
                 <h2 class="text-center mt-2">
                     Gerar Nova Senha
                 </h2>
-
                 <form action="#" method="post" id="formSenha" class="p-2">
-
                     <div class="form-group">
-
                         <small class="text-muted">
-                            Para gerar uma nova senha, digite o seu e-mail.
-                            Clique no link gerado.
+                            Para gerar uma nova senha, digite o
+                            seu e-mail. Clique no link gerado.
                         </small>
-
                     </div>
-
                     <div class="form-group">
-
                         <input type="email" name="emailGerarSenha" id="emailGerarSenha" class="form-control" placeholder="E-mail de recuperação de senha" required>
-
                     </div>
-
                     <div class="form-group">
-
-                        <input type="submit" value="Gerar" name="btnGerar" id="btnGerar" class="btn btn-primary btn-block">
-
+                        <input type="submit" value="::Gerar::" name="btnGerar" id="btnGerar" class="btn btn-primary btn-block">
                     </div>
-
                     <div class="form-group">
-
                         <p class="text-center">
-                            Já registrado? <a href="#" id="btnJaRegistrado">
+                            Já registrado?
+                            <a href="#" id="btnJaRegistrado">
                                 Entrar por aqui.
                             </a>
                         </p>
-
                     </div>
-
                 </form>
-
             </div>
-
         </section>
-
         <!-- Fim da Seção de Recuperação de Senha -->
-
-        <!-- Início do Formulário de Cadastro de novos usuários -->
-
+        <!-- Início do formulário de 
+        cadastro de novos usuários -->
         <section class="row mt-5">
-
             <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaRegistro">
-
                 <h2 class="text-center mt-2">Registre-se aqui</h2>
                 <form action="#" method="post" class="p-2" id="formRegistro">
-
                     <div class="form-group">
-
                         <input type="text" name="nomeCompleto" id="nomeCompleto" class="form-control" placeholder="Nome completo" required minlength="6">
-
                     </div>
-
                     <div class="form-group">
-
-                        <input type="text" name="nomeUsuario" id="nomeUsuario" class="form-control" placeholder="Nome de usuário" required minlength="5">
-
+                        <input type="text" name="nomeDoUsuario" id="nomeDoUsuario" class="form-control" placeholder="Nome de usuário" required minlength="5">
                     </div>
-
                     <div class="form-group">
-
                         <input type="email" name="emailUsuario" id="emailUsuario" class="form-control" placeholder="E-mail" required>
-
                     </div>
-
                     <div class="form-group">
-
                         <input type="password" name="senhaDoUsuario" id="senhaDoUsuario" class="form-control" placeholder="Senha" required minlength="6">
-
                     </div>
-
                     <div class="form-group">
-
-                        <input type="password" name="confirmacaoSenhaUsuario" id="confirmacaoSenhaUsuario" class="form-control" placeholder="Confirmação de Senha" required minlength="6">
-
+                        <input type="password" name="senhaUsuarioConfirmar" id="senhaUsuarioConfirmar" class="form-control" placeholder="Confirmar senha" required minlength="6">
                     </div>
-
                     <div class="form-group mt-4">
-
                         <div class="custom-control custom-checkbox">
-
                             <input type="checkbox" name="concordar" id="concordar" class="custom-control-input">
                             <label for="concordar" class="custom-control-label">
-                                Eu concordo com
-                                <a href="#">os termos e condições </a>
+                                Eu concordo com <a href="#">
+                                    os termos e condições.</a>
                             </label>
-
                         </div>
-
                     </div>
-
                     <div class="form-group">
-
-                        <input type="submit" value="Registrar" name="btnRegistrar" id="btnRegistrar" class="btn btn-primary btn-block">
-
+                        <input type="submit" value="::Registrar::" name="btnRegistrar" id="btnRegistrar" class="btn btn-primary btn-block">
                     </div>
-
                     <div class="form-group">
-
                         <p class="text-center">
                             Já registrado?
                             <a href="#" id="btnJaRegistrado2">
                                 Entrar por aqui.
                             </a>
                         </p>
-
                     </div>
-
                 </form>
-
             </div>
-
         </section>
-
-
-        <!-- Final do Formulário de novos usuários -->
-
+        <!-- Final do formulário de 
+        cadastro de novos usuários -->
     </main>
-
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
-
     <script>
-        // codigo jQuery para mostrar e ocultar os formulários
-
+        //Código jQuery para mostrar e ocultar os formulários
         $(function() {
-
-            //Validação de Formulários 
-
+            //Validação de Formulários
             jQuery.validator.setDefaults({
                 success: "valid"
             });
-            
             $("#formRegistro").validate({
                 rules: {
                     senhaDoUsuario: "required",
-                    confirmacaoSenhaUsuario: {
+                    senhaUsuarioConfirmar: {
                         equalTo: "#senhaDoUsuario"
                     }
                 }
             });
-
+            $("#formLogin").validate();
+            $("#formSenha").validate();
             //Mostrar e Ocultar Formulários
-
             $("#btnEsqueci").click(function() {
-                $("#caixaLogin").hide(); //Ocultar
-                $("#caixaSenha").show(); //Mostrar
+                $("#caixaLogin").hide(); //Ocultar Login
+                $("#caixaSenha").show(); //Mostrar Nova Senha
             });
-
             $("#btnJaRegistrado").click(function() {
-                $("#caixaSenha").hide(); //Ocultar
-                $("#caixaLogin").show(); //Mostrar
+                $("#caixaSenha").hide(); //Ocultar Gerar NovaSenha
+                $("#caixaLogin").show(); //Mostrar caixa Login
             });
-
             $("#btnRegistrarNovo").click(function() {
                 $("#caixaLogin").hide(); //Ocultar
                 $("#caixaRegistro").show(); //Mostrar
             });
-
             $("#btnJaRegistrado2").click(function() {
-                $("#caixaRegistro").hide(); //Ocultar
                 $("#caixaLogin").show(); //Mostrar
+                $("#caixaRegistro").hide(); //Ocultar
             });
-
+            //Cadastro de novo usuário
+            $("#btnRegistrar").click(function(e) {
+                if (document
+                    .querySelector("#formRegistro")
+                    .checkValidity()) {
+                    e.preventDefault(); //Não abrir outra págin
+                    //Envio dos dados via Ajax
+                    $.ajax({
+                        url: 'receber_dados.php',
+                        method: 'post',
+                        data: $("#formRegistro").serialize() + '&action=cadastro',
+                        success: function(resposta) {
+                            $("#alerta").show();
+                            $(".resultado").html(resposta);
+                        }
+                    });
+                }
+                return true;
+            });
+            //Login
+            $("#btnEntrar").click(function(e) {
+                if (document
+                    .querySelector("#formLogin")
+                    .checkValidity()) {
+                    e.preventDefault(); //Não abrir outra págin
+                    //Envio dos dados via Ajax
+                    $.ajax({
+                        url: 'receber_dados.php',
+                        method: 'post',
+                        data: $("#formLogin").serialize() + '&action=login',
+                        success: function(resposta) {
+                            $("#alerta").show();
+                            $(".resultado").html(resposta);
+                        }
+                    });
+                }
+                return true;
+            });
+            //Recuperação de senha
+            $("#btnGerar").click(function(e) {
+                if (document
+                    .querySelector("#formSenha")
+                    .checkValidity()) {
+                    e.preventDefault(); //Não abrir outra págin
+                    //Envio dos dados via Ajax
+                    $.ajax({
+                        url: 'receber_dados.php',
+                        method: 'post',
+                        data: $("#formSenha").serialize() + '&action=senha',
+                        success: function(resposta) {
+                            $("#alerta").show();
+                            $(".resultado").html(resposta);
+                        }
+                    });
+                }
+                return true;
+            });
         });
-
-        $("#formLogin").validate();
-
-        $("#formSenha").validate();
-
         /*
          * Translated default messages for the jQuery validation plugin.
          * Locale: PT_BR
@@ -316,7 +271,6 @@
             max: jQuery.validator.format("Por favor, forne&ccedil;a um valor menor ou igual a {0}."),
             min: jQuery.validator.format("Por favor, forne&ccedil;a um valor maior ou igual a {0}.")
         });
-
     </script>
 </body>
 
--- receber_dados.php
+++ receber_dados.php
@@ -1,22 +1,44 @@
+<?php
 //Teste se existe a ação
 
-if(isset($_POST['action']))
+if(isset($_POST['action'])){
 
     //Teste se ação é igual a cadastro
 
-    if($_POST['action']= 'cadastro'){
+    if($_POST['action'] == 'cadastro'){
 
         //Teste se ação é igual a cadastro
+        echo"\n<p>cadastro</p>";
+        echo"\n<pre>";
+        print_r($_POST);
+        echo "\n</pre";
 
-    }else if($_POST['action']='login'){
+    }else if($_POST['action'] =='login'){
 
         //Senão, teste se ação é login
+        echo "\n<p>login</p>";
+        echo "\n<pre>";
+        print_r($_POST);
+        echo "\n</pre";
 
-    }else if($_POST['action']='senha'){
+    }else if($_POST['action'] =='senha'){
 
         //Senão,teste se ação é recuperar senha
+        echo "\n<p>senha</p>";
+        echo "\n<pre>";
+        print_r($_POST);
+        echo "\n</pre";
 
     }else{
-        echo "<h1>Alo ha!</h1> <h2>Acesso negado</h2>";
+        
+        header("location:index.php");
+
+    }
+
+    }else{
+
+        //redirecionando para index.php, negando o acesso
+        //a esse arquivo diretamente 
+        header("location:index.php");
     }
 
