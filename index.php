1743018d43c473b0166a8d37188052cac46f321e
cadusoares <carlos_soares@estudante.sc.senai.br>
2019-10-29 00:40:07 -0200

    prova
    prova

--- index.php
+++ index.php
@@ -16,7 +16,8 @@
     <style>
         #alerta,
         #caixaSenha,
-        #caixaRegistro {
+        #caixaRegistro,
+        #caixaNovo {
             display: none;
         }
     </style>
@@ -29,12 +30,14 @@
             <div class="col-lg-4 offset-lg-4" id="alerta">
                 <div class="alert alert-success text-center">
                     <strong class="resultado">
-                    
+
                     </strong>
                 </div>
             </div>
         </section>
+
         <!-- Formulário de Login -->
+
         <section class="row">
             <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
                 <h2 class="text-center mt-2">
@@ -42,33 +45,25 @@
                 </h2>
                 <form action="#" method="post" class="p-2" id="formLogin">
                     <div class="form-group">
-                        <input type="text" name="nomeUsuario" id="nomeUsuario"
-                        placeholder="Nome de Usuário" class="form-control" 
-                        required minlength="5" value="<?= isset($_COOKIE['nomeDoUsuario'])
-                        ?$_COOKIE['nomeDoUsuario']:"";
-                        ?>">
+                        <input type="text" name="nomeUsuario" id="nomeUsuario" placeholder="Nome de Usuário" class="form-control" required minlength="5" value="<?= isset($_COOKIE['nomeDoUsuario'])
+                                                                                                                                                                    ? $_COOKIE['nomeDoUsuario'] : "";
+                                                                                                                                                                ?>">
 
-                        
+
                     </div>
                     <div class="form-group">
-                        <input type="password" name="senhaUsuario" 
-                        id="senhaUsuario" placeholder="Senha" 
-                        class="form-control"
-                        required minlength="6" value="<?= isset($_COOKIE['senhaDoUsuario'])
-                        ?$_COOKIE['senhaDoUsuario']:"";
-                        ?>">
+                        <input type="password" name="senhaUsuario" id="senhaUsuario" placeholder="Senha" class="form-control" required minlength="6" value="<?= isset($_COOKIE['senhaDoUsuario'])
+                                                                                                                                                                ? $_COOKIE['senhaDoUsuario'] : "";
+                                                                                                                                                            ?>">
 
                     </div>
                     <div class="form-group mt-5">
                         <div class="custom-control custom-checkbox">
-                            <input type="checkbox" name="lembrar" id="lembrar"
-                            class="custom-control-input"
-                            
-                            <?= 
-                            
-                            isset($_COOKIE['senhaDoUsuario'])?"checked":"";
-                            
-                            ?>>
+                            <input type="checkbox" name="lembrar" id="lembrar" class="custom-control-input" <?=
+
+                                                                                                                isset($_COOKIE['senhaDoUsuario']) ? "checked" : "";
+
+                                                                                                            ?>>
 
                             <label for="lembrar" class="custom-control-label">
                                 Lembrar de mim.
@@ -80,7 +75,7 @@
                     </div>
 
                     <div class="form-group">
-                        <input type="submit" value="::Entrar::" name="btnEntrar" id="btnEntrar" class="btn btn-primary btn-block">
+                        <input type="submit" value="Entrar" name="btnEntrar" id="btnEntrar" class="btn btn-primary btn-block">
                     </div>
                     <div class="form-group">
                         <p class="text-center">
@@ -89,86 +84,166 @@
                             </a>
                         </p>
                     </div>
+
+                    <div class="form-group">
+
+                        <p class="text-center">
+                            <a href="#" id="btnMostrar">
+                                Mostrar
+                            </a>
+                        </p>
+
+                    </div>
+
                 </form>
             </div>
         </section>
+
         <!-- Final da Seção de Login-->
-        <!-- Formulário de Recuperação de Senha -->
-        <section class="row mt-5">
-            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaSenha">
-                <h2 class="text-center mt-2">
-                    Gerar Nova Senha
-                </h2>
-                <form action="#" method="post" id="formSenha" class="p-2">
+
+        <!-- Formulário Novo-->
+
+        <section class="row mt-1">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded " id="caixaNovo">
+                <h2 class="text-center mt-2">Novo Formulario</h2>
+                <form action="#" method="post" class="p-2" id="formNovo">
                     <div class="form-group">
-                        <small class="text-muted">
-                            Para gerar uma nova senha, digite o
-                            seu e-mail. Clique no link gerado.
-                        </small>
+                        <label for="nomeCompleto">Nome Completo</label>
+                        <input type="text" name="nomeCompleto" id="nomeCompleto" required placeholder="Digite seu nome completo" class="form-control">
                     </div>
+
                     <div class="form-group">
-                        <input type="email" name="emailGerarSenha" id="emailGerarSenha" class="form-control" placeholder="E-mail de recuperação de senha" required>
+                        <label for="email">E-mail</label>
+                        <input type="email" name="email" id="email" required placeholder="Digite seu nome E-mail" class="form-control">
                     </div>
+
                     <div class="form-group">
-                        <input type="submit" value="::Gerar::" name="btnGerar" id="btnGerar" class="btn btn-primary btn-block">
+                        <label for="dataNiver">Data de Aniversario</label>
+                        <input type="date" name="dataNiver" id="dataNiver" required class="form-control">
                     </div>
+
                     <div class="form-group">
-                        <p class="text-center">
-                            Já registrado?
-                            <a href="#" id="btnJaRegistrado">
-                                Entrar por aqui.
-                            </a>
-                        </p>
+                        <label for="urlFace">Perfil do Facebook</label>
+                        <input type="url" name="urlFace" id="urlFace" required placeholder="Pagina do perfil do Facebook" class="form-control">
                     </div>
-                </form>
-            </div>
-        </section>
-        <!-- Fim da Seção de Recuperação de Senha -->
-        <!-- Início do formulário de 
-        cadastro de novos usuários -->
-        <section class="row mt-5">
-            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaRegistro">
-                <h2 class="text-center mt-2">Registre-se aqui</h2>
-                <form action="#" method="post" class="p-2" id="formRegistro">
+
                     <div class="form-group">
-                        <input type="text" name="nomeCompleto" id="nomeCompleto" class="form-control" placeholder="Nome completo" required minlength="6">
+                        <label for="urlImagem">Imagem de Perfil</label>
+                        <input type="url" name="urlImagem" id="urlImagem" required placeholder="Link da imagem do seu perfil" class="form-control">
                     </div>
+
                     <div class="form-group">
-                        <input type="text" name="nomeDoUsuario" id="nomeDoUsuario" class="form-control" placeholder="Nome de usuário" required minlength="5">
-                    </div>
-                    <div class="form-group">
-                        <input type="email" name="emailUsuario" id="emailUsuario" class="form-control" placeholder="E-mail" required>
-                    </div>
-                    <div class="form-group">
-                        <input type="password" name="senhaDoUsuario" id="senhaDoUsuario" class="form-control" placeholder="Senha" required minlength="6">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" name="senhaUsuarioConfirmar" id="senhaUsuarioConfirmar" class="form-control" placeholder="Confirmar senha" required minlength="6">
-                    </div>
-                    <div class="form-group mt-4">
-                        <div class="custom-control custom-checkbox">
-                            <input type="checkbox" name="concordar" id="concordar" class="custom-control-input">
-                            <label for="concordar" class="custom-control-label">
-                                Eu concordo com <a href="#">
-                                    os termos e condições.</a>
-                            </label>
+                        <label for="estado">Estado</label>
+                        <select class="form-control" name="estado" id="estado">
+                            <option></option>
+                            <option value="PR">Paraná</option>
+                            <option value="RS">Rio grande do sul</option>
+                            <option value="SC">Santa Catarina</option>
+                        </select>
+
+                        <div class="form-group">
+                            <label for="cidade">Cidade</label>
+                            <select class="form-control" name="cidade" id="cidade">
+                                <option></option>
+                                <option value="Brusque">Brusque</option>
+                                <option value="Guabiruba">Guabiruba</option>
+                                <option value="Itajai">Itajai</option>
+                                <option value="Gaspar">Gaspar</option>
+                                <option value="Botuvera">Botuvera</option>
+                                <option value="Nova trento">Nova trento</option>
+                            </select>
                         </div>
+
+                        <div class="form-group">
+                            <input type="submit" value="Entrar" name="btnEntrar" id="btnEntrar" class="btn btn-primary btn-block">
+                        </div>
+
+                        <div class="form-group">
+                            <input type="submit" value="Ínicio" name="btnVoltar" id="btnVoltar" class="btn btn-primary btn-block">
+                        </div>
+
+    </main>
+
+
+    <!-- Final do Novo Formulário-->
+
+    <!-- Formulário de Recuperação de Senha -->
+
+    <section class="row mt-5">
+        <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaSenha">
+            <h2 class="text-center mt-2">
+                Gerar Nova Senha
+            </h2>
+            <form action="#" method="post" id="formSenha" class="p-2">
+                <div class="form-group">
+                    <small class="text-muted">
+                        Para gerar uma nova senha, digite o
+                        seu e-mail. Clique no link gerado.
+                    </small>
+                </div>
+                <div class="form-group">
+                    <input type="email" name="emailGerarSenha" id="emailGerarSenha" class="form-control" placeholder="E-mail de recuperação de senha" required>
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="::Gerar::" name="btnGerar" id="btnGerar" class="btn btn-primary btn-block">
+                </div>
+                <div class="form-group">
+                    <p class="text-center">
+                        Já registrado?
+                        <a href="#" id="btnJaRegistrado">
+                            Entrar por aqui.
+                        </a>
+                    </p>
+                </div>
+            </form>
+        </div>
+    </section>
+    <!-- Fim da Seção de Recuperação de Senha -->
+    <!-- Início do formulário de 
+        cadastro de novos usuários -->
+    <section class="row mt-5">
+        <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaRegistro">
+            <h2 class="text-center mt-2">Registre-se aqui</h2>
+            <form action="#" method="post" class="p-2" id="formRegistro">
+                <div class="form-group">
+                    <input type="text" name="nomeCompleto" id="nomeCompleto" class="form-control" placeholder="Nome completo" required minlength="6">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="nomeDoUsuario" id="nomeDoUsuario" class="form-control" placeholder="Nome de usuário" required minlength="5">
+                </div>
+                <div class="form-group">
+                    <input type="email" name="emailUsuario" id="emailUsuario" class="form-control" placeholder="E-mail" required>
+                </div>
+                <div class="form-group">
+                    <input type="password" name="senhaDoUsuario" id="senhaDoUsuario" class="form-control" placeholder="Senha" required minlength="6">
+                </div>
+                <div class="form-group">
+                    <input type="password" name="senhaUsuarioConfirmar" id="senhaUsuarioConfirmar" class="form-control" placeholder="Confirmar senha" required minlength="6">
+                </div>
+                <div class="form-group mt-4">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" name="concordar" id="concordar" class="custom-control-input">
+                        <label for="concordar" class="custom-control-label">
+                            Eu concordo com <a href="#">
+                                os termos e condições.</a>
+                        </label>
                     </div>
-                    <div class="form-group">
-                        <input type="submit" value="Registrar" name="btnRegistrar" id="btnRegistrar" class="btn btn-primary btn-block">
-                    </div>
-                    <div class="form-group">
-                        <p class="text-center">
-                            Já registrado?
-                            <a href="#" id="btnJaRegistrado2">
-                                Entrar por aqui.
-                            </a>
-                        </p>
-                    </div>
-                </form>
-            </div>
-        </section>
-        <!-- Final do formulário de 
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Registrar" name="btnRegistrar" id="btnRegistrar" class="btn btn-primary btn-block">
+                </div>
+                <div class="form-group">
+                    <p class="text-center">
+                        Já registrado?
+                        <a href="#" id="btnJaRegistrado2">
+                            Entrar por aqui.
+                        </a>
+                    </p>
+                </div>
+            </form>
+        </div>
+    </section>
+    <!-- Final do formulário de 
         cadastro de novos usuários -->
     </main>
     <!-- Optional JavaScript -->
@@ -211,6 +286,15 @@
                 $("#caixaLogin").show(); //Mostrar
                 $("#caixaRegistro").hide(); //Ocultar
             });
+            $("#btnMostrar").click(function() {
+                $("#caixaNovo").show(); //Mostrar
+                $("#caixaLogin").hide(); //Ocultar
+            });
+            $("#btnVoltar").click(function() {
+                $("#caixaLogin").show(); //mostrar
+                $("#caixaNovo").hide(); //ocultar
+            });
+
 
             //Cadastro de novo usuário
             $("#btnRegistrar").click(function(e) {
@@ -246,11 +330,11 @@
                         success: function(resposta) {
                             $("#alerta").show();
                             //$(".resultado").html(resposta);
-                            if (resposta === "ok"){
+                            if (resposta === "ok") {
                                 window.location = "perfil.php";
                             } else {
                                 $(".resultado").html(resposta);
-                            } 
+                            }
                         }
                     });
                 }
